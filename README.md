af779e00e865bcf9641cd3cbc0b3ecefbdfdb0ea
rasheed <olanrewajurasheedopeyemi@gmail.com>
2022-06-23 05:17:14 +0100

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+#### Assignment
+
+Write a javascript code that prints out your name, height, and country on the screen
---
+++ app.js
@@ -0,0 +1,3 @@
+document.write("Name: OLANREWAJU Rasheed " + "<br>")
+document.write("Height: 150cm" + "<br>")
+document.write("Country: Nigeria")
